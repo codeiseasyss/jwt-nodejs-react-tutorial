b80289c21f4109fe119d9089c4fd9c34017640e8
codeiseasyss <21a120100302@students.hou.edu.vn>
2024-07-20 17:29:00 +0700

    Initial commit without LF to CRLF conversion

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/wep";
+require("dotenv").config();
+
+const app = express();
+//config view engine
+configViewEngine(app);
+
+//init web routes
+initWebRoutes(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(">>>JWT Backend is running on the PORT = " + PORT);
+})
