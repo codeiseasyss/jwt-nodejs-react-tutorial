ee37c5c29554773ed08e31766ed73e16b3c36e97
codeiseasyss <21a120100302@students.hou.edu.vn>
2024-08-11 10:43:02 +0700

    video 6.1 generate model/migration/seeder

--- src/server.js
+++ src/server.js
@@ -2,9 +2,8 @@ import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/wep";
 require("dotenv").config();
-
 import bodyParser from 'body-parser';
-
+//import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,6 +13,9 @@ configViewEngine(app);
 //config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connection
+//connection();
 
 //init web routes
 initWebRoutes(app);
