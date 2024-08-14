deb86757f98d9f3fb3d964b7afac72b28055a540
codeiseasyss <21a120100302@students.hou.edu.vn>
2024-08-14 10:17:48 +0700

    orm create video 7.0

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import mysql from 'mysql2/promise';
 import bluebird from 'bluebird';
+import db from '../models/index';
 
 
 const salt = bcrypt.genSaltSync(10);
@@ -15,19 +16,12 @@ const hashUserPassword = (userPassword) =>
 const createNewUser =  async(email, password, username) => 
 {
     let hashPass = hashUserPassword(password);
-        
-    // create the connection, specify bluebird as Promise
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'jwt',
-        Promise: bluebird,
-    });
-
     try {
-        const [rows, fields] = 
-            await connection.execute ( 'INSERT INTO user (email, password, username) VALUES (?, ?, ?)',  
-                [email, hashPass, username] );
+        await db.User.create({
+            username: username,
+            email: email,
+            password: hashPass
+        })
     }
     catch(errol) {
         console.log("check errol:  ", errol);
